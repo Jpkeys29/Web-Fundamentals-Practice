c5506be2c8973957cc5aa3946c260790e70c5bc8
Jpkeys29 <98492814+Jpkeys29@users.noreply.github.com>
2022-03-01 17:40:47 -0500

    Add files via upload
    Dojo Pizzeria Code

---
+++ pizza.js
@@ -0,0 +1,20 @@
+function pizzaOven (crustType, sauceType, cheeses, toppings){
+    var pizza = {
+    crustType : crustType,
+    sauceType : sauceType,
+    cheeses : cheeses,
+    toppings : toppings,
+    }
+    return pizza
+}
+console.log(pizzaOven ("thin", "pesto", "provole", "prosciuto"))
+console.log(pizzaOven ("deep dish", "traditional", "mozzarella", "pepperoni"))
+console.log(pizzaOven ("hand tossed", "marinara", "mozzarella", "feta"))
+
+    
+
+
+
+
+
+
